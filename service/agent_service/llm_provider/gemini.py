4bc890f16516efc4e0d965f5c1f79f14bdaf3174
BangPham18 <pbang4589@gmail.com>
2025-08-14 15:59:33 +0700

    routes dang ky

--- service/agent_service/llm_provider/gemini.py
+++ service/agent_service/llm_provider/gemini.py
@@ -3,7 +3,7 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from app.core.config import settings
 
 llm = ChatGoogleGenerativeAI(
-    model="gemini-2.5-pro",
+    model="gemini-2.5-flash",
     google_api_key=settings.GOOGLE_API_KEY,
     temperature=1.0,
 )
